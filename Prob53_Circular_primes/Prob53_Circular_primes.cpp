3cc6a9165072c69273cfa50dbbdb5f79dc6dcc5b
Jeffrey Joseph <jjrzacal@outlook.com>
2019-09-18 13:26:39 -0700

    final?

--- Prob53_Circular_primes/Prob53_Circular_primes.cpp
+++ Prob53_Circular_primes/Prob53_Circular_primes.cpp
@@ -4,13 +4,27 @@
 #include "pch.h"
 #include <iostream>
 
+int CheckPrime(unsigned int n)
+{
+	int i;
+	float sr;
+	sr = sqrtf(n);
+	for (i = 2; i < sr && n % i != 0; i++);
+	if (i > sr)
+		return 1; // not 0 true
+	return 0;
+	// 0 is false
+}
+
 int main()
 {
-	unsigned int i, j, n, digit;
+	unsigned int i, j, n, digit, temp;
 	float sr;
-	for (i = 1; i < 1000000; i++)
+	int prime, digitcount, factor, ctr, primectr = 2; // primectr = 0 (initital) + 2 (algo disregards 2 and 5 )
+	//55
+	for (i = 2; i < 1000000; i++)
 	{
-		if (i % 2 != 0)
+		if (i % 2 != 0 && i % 5 != 0)
 		{
 			sr = sqrtf(i);
 			for (j = 3; j <= sr && i % j != 0; j++);
@@ -25,10 +39,62 @@ int main()
 					n = n / 10;
 				}
 				if (n <= 0)
+				{
+					//no number with even digit is found here
 					printf("\n%d", i);
+					prime = 1;
+					if (i >= 100000)
+					{
+						digitcount = 5;
+						factor = 1000000;
+					}
+					else if (i >= 10000)
+					{
+						digitcount = 4;
+						factor = 100000;
+					}
+					else if (i >= 1000)
+					{
+						digitcount = 3;
+						factor = 10000;
+					}
+					else if (i >= 100)
+					{
+						digitcount = 2;
+						factor = 1000;
+					}
+					else if (i >= 10)
+					{
+						digitcount = 1;
+						factor = 100;
+					}
+					else
+					{
+						digitcount = 0;
+					}
+					n = i;
+					for (ctr = 0; ctr < digitcount; ctr++)
+					{
+						temp = (n % 10) * factor;
+						n = n + temp;
+						n = n / 10;
+
+						//check in n is also prime
+						prime = prime && CheckPrime(n);
+						printf(" => %d", n);
+					}
+					if (prime)
+					{
+						printf("*");
+						primectr++;
+					}
+						
+				}
+					
 			}
 		}
 	}
+	printf("\n\n%d", primectr);
 }
 
 /*
